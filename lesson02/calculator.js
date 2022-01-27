e912456b48fffc72dc108bb2e4c7b34677cef905
Allison <bondy.allison@gmail.com>
2022-01-27 15:23:52 -0800

    Adding a change.

---
+++ lesson02/calculator.js
@@ -0,0 +1,32 @@
+const x = 1;
+const y = 2;
+let z = x + y;
+
+console.log(z);
+
+3
+
+
+const x = 'I';
+const y = 'like';
+const z = 'coffee';
+
+console.log(x + ' ' + y + ' ' + z);
+
+"I like coffee"
+
+
+let x = 5;
+let y = 10;
+let z = (x + y) * 3;
+
+console.log(z);
+
+45
+
+const wordOne = 'myfirstName';
+const wordTwo = 'mysurName';
+
+console.log(wordOne + ' ' + wordTwo);
+
+"myfirstName mysurName"
